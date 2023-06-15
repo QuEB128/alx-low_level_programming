1437f373886cca6539e2e6f227c14cc5128580ce
QuEB128 <equartey128@gmail.com>
2023-06-15 06:03:43 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line with a using the puts function
+ * Return: 0 
+ */
+int main(void) {
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
