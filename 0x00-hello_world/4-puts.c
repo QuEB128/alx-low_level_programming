68cbcdba58d3130965bc13e33a6cc93fcd640cec
QuEB128 <equartey128@gmail.com>
2023-06-15 06:06:30 -0700

    betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * main - A program that print a line with a using the puts function
  * Return: 0 
  */
-int main(void) {
+int main(void) 
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
