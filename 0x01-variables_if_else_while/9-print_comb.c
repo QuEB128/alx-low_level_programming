1b4490f89e5b261d71ea4f71d1cf11bea427bcca
QuEB128 <equartey128@gmail.com>
2023-06-16 08:22:15 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,28 +10,24 @@
  */
 int main(void)
 {
-    int i, j;
-
-    for (i = 0; i < 10; i++)
-    {
-        for (j = i; j < 10; j++)
-        {
-            putchar((i / 10) + '0');
-            putchar((i % 10) + '0');
-            putchar(',');
-            putchar(' ');
-            putchar((j / 10) + '0');
-            putchar((j % 10) + '0');
-
-            if (i != 9 || j != 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-
-    putchar('\n');
-
-    return (0);
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = i; j < 10; j++)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(',');
+putchar(' ');
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
+if (i != 9 || j != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+}
+return (0);
 }
