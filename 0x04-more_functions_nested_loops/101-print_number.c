b2e9b45ceee740fe2186c293f10edb90ff3873cb
QuEB128 <equartey128@gmail.com>
2023-06-22 14:50:16 -0700

    la fin

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,35 +1,43 @@
 #include "main.h"
 
 /**
-* print_number - prints an integer using only putchar
-* @n: the integer to be printed
+
+* print_number - prints an integer
+
+* @n: integer to be printed
+
 */
+
 void print_number(int n)
+
 {
-    int divisor = 1;
-    int i, digit, negative = 0;
 
-    if (n == 0)
-    {
-        _putchar('0');
-        return;
-    }
+unsigned int n1;
 
-    if (n < 0)
-    {
-        _putchar('-');
-        negative = 1;
-        n = -n;
-    }
+if (n < 0)
 
-    /* Find the largest power of 10 that divides n */
-    while (n / divisor >= 10)
-        divisor *= 10;
+{
 
-    /* Print each digit of n in turn */
-    for (i = divisor; i >= 1; i /= 10)
-    {
-        digit = (n / i) % 10;
-        _putchar('0' + digit);
-    }
+n1 = -n;
+
+_putchar('-');
+
+} else
+
+{
+
+n1 = n;
+
+}
+
+if (n1 / 10)
+
+{
+
+print_number(n1 / 10);
+
+}
+
+_putchar((n1 % 10) + '0');
+
 }
