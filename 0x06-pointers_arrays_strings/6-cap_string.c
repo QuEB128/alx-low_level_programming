29828ae85d83bef8051f0be5873caeeffdbc559d
QuEB128 <equartey128@gmail.com>
2023-06-29 07:01:40 -0700

    str caps

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * cap_string - Capitalizes all words of a string
+ * @s: The string to be modified
+ *
+ * Return: A pointer to the resulting string s
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	/* Capitalize first letter */
+	if (s[0] >= 'a' && s[0] <= 'z')
+	{
+	s[0] -= 32;
+	}
+	/* Capitalize subsequent letters after separators */
+	for (i = 1; s[i] != '\0'; i++)
+	{
+	if (s[i] >= 'a' && s[i] <= 'z')
+	{
+	if (s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n' ||
+	s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+	s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+	s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
+	s[i - 1] == '}')
+	{
+	s[i] -= 32;
+	}
+	}
+	}
+	return (s);
+}
