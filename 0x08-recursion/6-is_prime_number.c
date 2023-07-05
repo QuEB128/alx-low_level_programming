e6b399a6c1e5716005d1ea8c83a62fe2af45e49a
QuEB128 <equartey128@gmail.com>
2023-07-05 04:07:47 -0700

    PrimeNums

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,22 @@
-#include <stdio.h>
 #include "main.h"
+
+/**
+ * is_prime_number_helper - checks if a number is prime recursively
+ * @n: the number to check
+ * @i: the divisor to try
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+
+int is_prime_number_helper(int n, int i)
+{
+if (n <= 1 || (i > 1 && i < n && n % i == 0))
+return (0);
+else if (i == n)
+return (1);
+else
+return (is_prime_number_helper(n, i + 1));
+}
 
 /**
  * is_prime_number - checks if a number is prime
@@ -7,30 +24,8 @@
  *
  * Return: 1 if n is prime, 0 otherwise
  */
+
 int is_prime_number(int n)
 {
-if (n <= 1)
-{
-return (0);
-}
-else if (n == 2)
-{
-return (1);
-}
-else if (n % 2 == 0)
-{
-return (0);
-}
-else
-{
-int i;
-for (i = 3; i * i <= n; i += 2)
-{
-if (n % i == 0)
-{
-return (0);
-}
-}
-return (1);
-}
+return (is_prime_number_helper(n, 2));
 }
