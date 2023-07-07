a0f45f59fc86a8d39b25b423dbe4dcf0d1b43366
QuEB128 <equartey128@gmail.com>
2023-07-07 09:01:35 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: the number of command-line arguments
+ * @argv: an array of pointers to the arguments
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char **argv)
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
